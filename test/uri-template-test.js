f26875946378de64275f96e485d74965496b171c
Bram Stein <b.l.stein@gmail.com>
2012-10-30 18:51:05 +0100

    Added a test to ensure surrogate pairs are handled correctly.

--- test/uri-template-test.js
+++ test/uri-template-test.js
@@ -22,7 +22,8 @@ describe('uri-template', function () {
                 'float': 3.14,
                 'undef': undefined,
                 'null': null,
-                'chars': 'šöäŸœñê€£¥‡ÑÒÓÔÕÖ×ØÙÚàáâãäåæçÿü'
+                'chars': 'šöäŸœñê€£¥‡ÑÒÓÔÕÖ×ØÙÚàáâãäåæçÿü',
+                'surrogatepairs': '\uD834\uDF06'
             });
 
         it('empty string', function () {
@@ -44,6 +45,10 @@ describe('uri-template', function () {
 
         it('expand non-ASCII strings', function () {
             assert('{chars}', '%C5%A1%C3%B6%C3%A4%C5%B8%C5%93%C3%B1%C3%AA%E2%82%AC%C2%A3%C2%A5%E2%80%A1%C3%91%C3%92%C3%93%C3%94%C3%95%C3%96%C3%97%C3%98%C3%99%C3%9A%C3%A0%C3%A1%C3%A2%C3%A3%C3%A4%C3%A5%C3%A6%C3%A7%C3%BF%C3%BC');
+        });
+
+        it('expands and encodes surrogate pairs correctly', function () {
+            assert('{surrogatepairs}', '%F0%9D%8C%86');
         });
 
         it('expand expressions with dot and underscore', function () {
