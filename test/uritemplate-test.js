f6badabc6372c753a1f120592c01c74a54db5f99
Jon Koops <jonkoops@gmail.com>
2022-01-28 10:38:32 +0100

    Replace expect.js with Chai (#33)

--- test/uritemplate-test.js
+++ test/uritemplate-test.js
@@ -2,11 +2,11 @@ var template, expect, examples;
 
 if (typeof require !== 'undefined') {
   template = require('../lib/url-template.js');
-  expect = require("expect.js");
+  expect = require("chai").expect;
   examples = require('../uritemplate-test/spec-examples-by-section.json');
 } else {
   template = window.urltemplate;
-  expect = window.expect;
+  expect = window.chai.expect;
   examples = window.examples;
 }
 
@@ -15,7 +15,7 @@ function createTestContext(c) {
     if (typeof r === 'string') {
       expect(template.parse(t).expand(c)).to.eql(r);
     } else {
-      expect(r.indexOf(template.parse(t).expand(c)) >= 0).to.be.ok();
+      expect(r.indexOf(template.parse(t).expand(c)) >= 0).to.be.true;
     }
   };
 }
