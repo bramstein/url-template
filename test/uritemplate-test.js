06c92e924480ce29b5fbeb72133e50cbcc046760
Jon Koops <jonkoops@gmail.com>
2022-01-28 12:10:49 +0100

    Use a named export for the parse function

--- test/uritemplate-test.js
+++ test/uritemplate-test.js
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
-import template from '../lib/url-template.js';
+import { parseTemplate } from '../lib/url-template.js';
 import examples from '../uritemplate-test/spec-examples-by-section.json';
 
 function createTestContext(c) {
   return function (t, r) {
     if (typeof r === 'string') {
-      expect(template.parse(t).expand(c)).to.eql(r);
+      expect(parseTemplate(t).expand(c)).to.eql(r);
     } else {
-      expect(r.indexOf(template.parse(t).expand(c)) >= 0).to.be.true;
+      expect(r.indexOf(parseTemplate(t).expand(c)) >= 0).to.be.true;
     }
   };
 }
