fcecd7b7ae3aa22d1371d82c76de85f824423774
Jon Koops <jonkoops@gmail.com>
2023-12-13 18:35:02 +0100

    Allow nested array values for `expand()` in type definition (#70)

--- lib/url-template.d.ts
+++ lib/url-template.d.ts
@@ -1,7 +1,7 @@
 export type PrimitiveValue = string | number | boolean | null;
 
 export interface Template {
-  expand(context: Record<string, PrimitiveValue | PrimitiveValue[] | Record<string, PrimitiveValue>>): string;
+  expand(context: Record<string, PrimitiveValue | PrimitiveValue[] | Record<string, PrimitiveValue | PrimitiveValue[]>>): string;
 }
 
 export function parseTemplate(template: string): Template;
